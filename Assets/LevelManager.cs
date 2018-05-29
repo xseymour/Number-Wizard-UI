ff8d44e4c65f547be8e372b8702a33d8f6257258
Xavier Seymour <xseymour3@gmail.com>
2018-05-29 09:38:28 -0400

    Loading Scenes

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest() {
+		Application.Quit();
+	}
+	
+}
